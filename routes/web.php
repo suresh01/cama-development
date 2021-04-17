4cd77c98c89d705086d938fbc04eb36fd136a79b
sureshkumar <createtosuresh@gmail.com>
2021-04-17 12:52:28 +0530

    Update 10

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ Route::get('termattachment','InspectionController@termAttachment');
 
 Route::get('termbasket','HomeController@termBasket')->name('termbasket');
 
-Route::get('testbasket','HomeController@testBasket');
+Route::get('testbasket','UserController@testBasket');
 
 /*Route::get('dashboard', function () {
     return view('dashboard'); 
